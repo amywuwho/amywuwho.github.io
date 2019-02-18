1556b60f2a9a0b45dfcc551cd4704a53c51ba1d3
amywuwho <amy.wu1579@gmail.com>
2019-02-17 23:08:59 -0500

    bugs1

--- p5/you-are-in-a-room/main.js
+++ p5/you-are-in-a-room/main.js
@@ -24,11 +24,13 @@ function setup() {
 
     rm = new RiMarkov(4);
     rm.loadText(z.join(' '));
+
+    drawText();
 }
 
-function draw() {
+function drawText() {
     background(253, 246, 227);
-    text("You are in a" + location, windowWidth/2, windowHeight/2);
+    text("You are in a " + location, windowWidth/2, windowHeight/2);
 
     // text(lines.join(' '), windowWidth/2, windowHeight/2);
 }
@@ -37,4 +39,5 @@ function mouseClicked() {
     // lines = rm.generateSentences(4);
     
     location = rg.expand();
+    drawText();
 }
