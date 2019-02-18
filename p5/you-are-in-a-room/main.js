c733447e7a218c2b1a96466722e4b10131ebdaa9
amywuwho <amy.wu1579@gmail.com>
2019-02-17 23:17:36 -0500

    ???????

--- p5/you-are-in-a-room/main.js
+++ p5/you-are-in-a-room/main.js
@@ -1,10 +1,52 @@
-var rm, lines, rg;
+// var rm, lines, rg;
+// var z;
+// var places, location;
+
+// function preload() {
+//     z = loadStrings("zork.txt");
+//     // places = loadStrings("places.yml");
+// }
+
+// function setup() {
+
+//     createCanvas(windowWidth, windowHeight);
+//     background(253, 246, 227);
+//     fill(48, 63, 71);
+//     textSize(50);
+//     textAlign(CENTER, CENTER);
+//     noStroke();
+
+//     lines = ["You are in a room."];
+
+//     // rg = new RiGrammar(places.join('\n'));
+//     location = "room";
+
+
+//     rm = new RiMarkov(4);
+//     rm.loadText(z.join(' '));
+
+//     drawText();
+// }
+
+// function drawText() {
+//     background(253, 246, 227);
+//     // text("You are in a " + location, windowWidth/2, windowHeight/2);
+
+//     text(lines.join(' '), windowWidth/2, windowHeight/2);
+// }
+
+// function mouseClicked() {
+//     lines = rm.generateSentences(4);
+    
+//     // location = rg.expand();
+//     drawText();
+// }
+
+var rm, lines;
 var z;
-var places, location;
 
 function preload() {
     z = loadStrings("zork.txt");
-    // places = loadStrings("places.yml");
 }
 
 function setup() {
@@ -16,11 +58,7 @@ function setup() {
     textAlign(CENTER, CENTER);
     noStroke();
 
-    lines = ["You are in a room."];
-
-    // rg = new RiGrammar(places.join('\n'));
-    location = "room";
-
+    lines = ["click to re-generate"];
 
     rm = new RiMarkov(4);
     rm.loadText(z.join(' '));
@@ -30,14 +68,10 @@ function setup() {
 
 function drawText() {
     background(253, 246, 227);
-    // text("You are in a " + location, windowWidth/2, windowHeight/2);
-
     text(lines.join(' '), windowWidth/2, windowHeight/2);
 }
 
 function mouseClicked() {
-    lines = rm.generateSentences(4);
-    
-    // location = rg.expand();
+    lines = rm.generateSentences(2);
     drawText();
 }
