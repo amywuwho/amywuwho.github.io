33bb253ae59d07e0e9888f6704d71f1025ff7ac6
amywuwho <amy.wu1579@gmail.com>
2019-02-17 19:17:30 -0500

    you-are-in-a-room
    try to add you are in a room as a second page

---
+++ p5/you-are-in-a-room/main.js
@@ -0,0 +1,44 @@
+var rm, lines;
+var data;
+// var ctt, ps, sh, tlh, z;
+
+function preload() {
+  data = loadStrings("genesis.txt");
+    // ctt = loadStrings("color_the_truth.txt");
+    // ps = loadStrings("problem_sleuth.txt");
+    // sh = loadStrings("stone_harbor.txt");
+    // tlh = loadStrings("the_lurking_horror.txt");
+    // z = loadStrings("zork.txt");
+
+}
+
+function setup() {
+
+    createCanvas(windowWidth, windowHeight);
+    background(253, 246, 227);
+    fill(48, 63, 71);
+    textSize(50);
+    textAlign(CENTER, CENTER);
+    noStroke();
+
+    lines = ["click to generate"];
+
+    rm = new RiMarkov(4);
+    // rm.loadText(ctt.join(' '));
+    // rm.loadText(ps.join(' '));
+    // rm.loadText(sh.join(' '));
+    // rm.loadText(tlh.join(' '));
+    // rm.loadText(z.join(' '));
+    rm.loadText(data.join(' '));
+
+    drawText();
+}
+
+function drawText() {
+    text(lines.join(' '), windowWidth/2, windowHeight/2);
+}
+
+function mouseClicked() {
+    lines = rm.generateSentences(2);
+    drawText();
+}
