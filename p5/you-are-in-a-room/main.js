6be04fd3b068c20477114f45584cd8b7a0d29883
amywuwho <amy.wu1579@gmail.com>
2019-02-19 23:40:14 -0500

    attempt to implement inventory

--- p5/you-are-in-a-room/main.js
+++ p5/you-are-in-a-room/main.js
@@ -2,16 +2,52 @@ var rm;
 var z;
 var places, rg;
 var input;
-var cur_room, start_room;
+var cur_room, start_room, you;
 var lines_margin;
 var cnv;
 var nlp = window.nlp_compromise;
 
 /* ------------------------- CLASS DEFINITIONS ------------------------- */
+class Character {
+    constructor() {
+        this.inventory = {}; // key is object, value is desc
+    }
+
+    take(thing, place) {
+        if (place.objects.includes(thing)) {
+            var i = places.objects.indexOf(thing);
+            var obj_sentence = "";
+            place.objects.splice(i, 1);
+
+            for (var j = 0; j < places.desc.length; j++) {
+                if (places.desc[j].indexOf(thing) !== -1) {
+                    obj_sentence += places.desc[j];
+                    places.desc.splice(j, 1);
+                }
+            }
+            this.inventory[thing] = obj_sentence;
+
+            console.log(place.objects);
+            console.log(this.inventory);
+        }
+    }
+
+    put(thing, place) {
+        if (this.inventory.includes(thing)) {
+            place.objects += thing;
+            place.desc += this.inventory[thing];
+            delete this.inventory[thing];
+
+            console.log(place.objects);
+            console.log(this.inventory);
+        }
+    }
+}
+
 class Room {
     constructor(name) {
         this.title = name;
-        this.desc = "";
+        this.desc = [];
 
         this.north = null;
         this.east = null;
@@ -22,7 +58,7 @@ class Room {
     }
 
     move(dir) {
-        if (dir === "north") {
+        if (dir === "north" || dir === "up") {
             if (this.north != null) cur_room = this.north;
             else {
                 var new_title = rg.expand();
@@ -34,7 +70,7 @@ class Room {
                 cur_room = new_room;
             }
         }
-        else if (dir === "south") {
+        else if (dir === "south" || dir === "down") {
             if (this.south != null) cur_room = this.south;
             else {
                 var new_title = rg.expand();
@@ -46,7 +82,7 @@ class Room {
                 cur_room = new_room;
             }
         }
-        else if (dir === "east") {
+        else if (dir === "east" || dir === "right") {
             if (this.east != null) cur_room = this.east;
             else {
                 var new_title = rg.expand();
@@ -58,7 +94,7 @@ class Room {
                 cur_room = new_room;
             }
         }
-        else if (dir === "west") {
+        else if (dir === "west" || dir === "left") {
             if (this.west != null) cur_room = this.west;
             else {
                 var new_title = rg.expand();
@@ -79,7 +115,7 @@ class Room {
 
     populate() {
         var sentences = rm.generateSentences(4);
-        this.desc = sentences.join(' ');
+        this.desc = sentences;
         
         var objs = nlp(this.desc).nouns().out('array');
         this.objects = objs;
@@ -137,6 +173,7 @@ function setup() {
 
     start_room = new Room("room");
     cur_room = start_room;
+    you = new Character();
 
     rg = new RiGrammar();
     rg.loadFrom("places.yml");
@@ -159,7 +196,7 @@ function drawText() {
          cur_room.title + ".", width/2, height/5);
 
     textAlign(LEFT, TOP);
-    text(cur_room.desc, lines_margin, height/3, width-2*lines_margin, height/2);
+    text(cur_room.desc.join(' '), lines_margin, height/3, width-2*lines_margin, height/2);
 }
 
 function keyPressed() {
@@ -169,9 +206,31 @@ function keyPressed() {
         const cmd_tokens = RiTa.tokenize(input_cmd);
 
         /* ------------------------- ACTIONS ------------------------- */
-        if (cmd_tokens[0] === "move") cur_room.move(cmd_tokens[1]);
+        // HARDCODE A BUNCH OF DIRECTIONS THEY COULD ASK TO MOVE IN
+        if (cmd_tokens[0] === "move" ||
+            cmd_tokens[0] === "go" ||
+            cmd_tokens[0] === "walk"
+        ) cur_room.move(cmd_tokens[1]);
 
-        // try to return something based on what they say
+        else if (
+            cmd_tokens[0] === "north" ||
+            cmd_tokens[0] === "west" ||
+            cmd_tokens[0] === "east" ||
+            cmd_tokens[0] === "south" ||
+            cmd_tokens[0] === "up" ||
+            cmd_tokens[0] === "left" ||
+            cmd_tokens[0] === "down" ||
+            cmd_tokens[0] === "right"
+        ) cur_room.move(cmd_tokens[0]);
+
+        else if (cmd_tokens[0] === "put" ||
+                 cmd_tokens[0] === "throw") 
+                you.put(cmd_tokens[1]);
+        else if (cmd_tokens[0] === "take" ||
+                 cmd_tokens[1] === "pick") 
+                you.take(cmd_tokens[1]);
+
+        // just make it smarter if they don't have...anything
         else rm.loadText(input_cmd, 3);
         /* ----------------------------------------------------------- */
 
