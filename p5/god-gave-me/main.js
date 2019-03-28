b7a0d3b1dd92929e5225d7aacd1bba8eacc4596e
amywuwho <amy.wu1579@gmail.com>
2019-03-28 03:47:06 -0400

    try to load directly into array

--- p5/god-gave-me/main.js
+++ p5/god-gave-me/main.js
@@ -13,96 +13,49 @@ function preload() {
     // image loads
 
     // head
-    bowlingball_head = loadImage("assets/bowlingball_head.png");
-    cat_head = loadImage("assets/cat_head.png");
-    dog_head = loadImage("assets/dog_head.png");
-    fish_head = loadImage("assets/fish_head.png");
-    old_man_head = loadImage("assets/old_man_head.png");
-    pinocchio_head = loadImage("assets/pinocchio_head.png");
-    real_pinocchio_head = loadImage("assets/real_pinocchio_head.png");
+    heads[0] = loadImage("assets/cat_head.png");
+    heads[1] = loadImage("assets/bowlingball_head.png");
+    heads[2] = loadImage("assets/dog_head.png");
+    heads[3] = loadImage("assets/fish_head.png");
+    heads[4] = loadImage("assets/old_man_head.png");
+    heads[5] = loadImage("assets/pinocchio_head.png");
+    heads[6] = loadImage("assets/real_pinocchio_head.png");
     
     // torso
-    bikini_torso = loadImage("assets/bikini_torso.png");
-    bikini_piercings_torso = loadImage("assets/bikini_piercings_torso.png");
-    buff_piercings_torso = loadImage("assets/buff_piercings_torso.png");
-    buff_torso = loadImage("assets/buff_torso.png");
-    cat_torso = loadImage("assets/cat_torso.png");
-    free_donuts_torso = loadImage("assets/free_donuts_torso.png");
-    gingerbread_torso = loadImage("assets/gingerbread_torso.png");
-    gingerbread_eaten_torso = loadImage("assets/gingerbread_torso.png");
-    swimmer_torso = loadImage("assets/swimmer_torso.png");
+    torsos[0] = loadImage("assets/cat_torso.png");
+    torsos[1] = loadImage("assets/bikini_torso.png");
+    torsos[2] = loadImage("assets/bikini_piercings_torso.png");
+    torsos[3] = loadImage("assets/buff_piercings_torso.png");
+    torsos[4] = loadImage("assets/buff_torso.png");
+    torsos[5] = loadImage("assets/free_donuts_torso.png");
+    torsos[6] = loadImage("assets/gingerbread_torso.png");
+    torsos[7] = loadImage("assets/gingerbread_torso.png");
+    torsos[8] = loadImage("assets/swimmer_torso.png");
 
     // bottom
-    cat_bottom = loadImage("assets/cat_bottom.png");
-    legwarmers_bottom = loadImage("assets/legwarmers_bottom.png");
-    mermaid_bottom = loadImage("assets/mermaid_bottom.png");
-    michaeljackson_bottom = loadImage("assets/michaeljackson_bottom.png");
-    robot_bottom = loadImage("assets/robot_bottom.png");
-    towel_bottom = loadImage("assets/towel_bottom.png");
+    bottoms[0] = loadImage("assets/cat_bottom.png");
+    bottoms[1] = loadImage("assets/legwarmers_bottom.png");
+    bottoms[2] = loadImage("assets/mermaid_bottom.png");
+    bottoms[3] = loadImage("assets/michaeljackson_bottom.png");
+    bottoms[4] = loadImage("assets/robot_bottom.png");
+    bottoms[5] = loadImage("assets/towel_bottom.png");
 
     // right
-    cat_right = loadImage("assets/cat_right.png");
-    dab_right = loadImage("assets/dab_right.png");
-    goodhotdog_right = loadImage("assets/goodhotdog_right.png");
-    octopus_right = loadImage("assets/octopus_right.png");
-    sadweiner_right = loadImage("assets/sadweiner_right.png");
-    watch_right = loadImage("assets/watch_right.png");
-    tie_right = loadImage("assets/tie_right.png");
+    rights[0] = loadImage("assets/cat_right.png");
+    rights[1] = loadImage("assets/dab_right.png");
+    rights[2] = loadImage("assets/goodhotdog_right.png");
+    rights[3] = loadImage("assets/octopus_right.png");
+    rights[4] = loadImage("assets/sadweiner_right.png");
+    rights[5] = loadImage("assets/watch_right.png");
+    rights[6] = loadImage("assets/tie_right.png");
 
     // left
-    cat_left = loadImage("assets/cat_left.png");
-    dab_left = loadImage("assets/dab_left.png");
-    donut_left = loadImage("assets/donut_left.png");
-    finger_left = loadImage("assets/finger_left.png");
-    selfie_left = loadImage("assets/selfie_left.png");
-    snap_left = loadImage("assets/snap_left.png");
-
-    
-    // image arrays
-    // head
-    heads[0] = bowlingball_head;
-    heads[1] = cat_head;
-    heads[2] = dog_head;
-    heads[3] = fish_head;
-    heads[4] = old_man_head;
-    heads[5] = pinocchio_head;
-    heads[6] = real_pinocchio_head;
-    
-    // torso
-    torsos[7] = bikini_torso;
-    torsos[8] = bikini_piercings_torso;
-    torsos[9] = buff_piercings_torso;
-    torsos[10] = buff_torso;
-    torsos[11] = cat_torso;
-    torsos[12] = free_donuts_torso;
-    torsos[13] = gingerbread_torso;
-    torsos[14] = gingerbread_eaten_torso;
-    torsos[15] = swimmer_torso;
-
-    // bottom
-    bottoms[16] = cat_bottom;
-    bottoms[17] = legwarmers_bottom;
-    bottoms[18] = mermaid_bottom;
-    bottoms[19] = michaeljackson_bottom;
-    bottoms[20] = robot_bottom;
-    bottoms[21] = towel_bottom;
-
-    // right
-    rights[22] = cat_right;
-    rights[23] = dab_right;
-    rights[24] = goodhotdog_right;
-    rights[25] = octopus_right;
-    rights[26] = sadweiner_right;
-    rights[27] = watch_right;
-    rights[28] = tie_right;
-
-    // left
-    lefts[29] = cat_left;
-    lefts[30] = dab_left;
-    lefts[31] = donut_left;
-    lefts[32] = finger_left;
-    lefts[33] = selfie_left;
-    lefts[34] = snap_left;
+    lefts[0] = loadImage("assets/cat_left.png");
+    lefts[1] = loadImage("assets/dab_left.png");
+    lefts[2] = loadImage("assets/donut_left.png");
+    lefts[3] = loadImage("assets/finger_left.png");
+    lefts[4] = loadImage("assets/selfie_left.png");
+    lefts[5] = loadImage("assets/snap_left.png");
 }
 
 function setup() {
