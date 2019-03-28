2c6850e1e9fa2f2a2259a5eae3326eebdc0e21e0
amywuwho <amy.wu1579@gmail.com>
2019-03-28 04:16:53 -0400

    add click system?

--- p5/god-gave-me/main.js
+++ p5/god-gave-me/main.js
@@ -6,6 +6,7 @@ var rights = [];
 var bottoms = [];
 
 var head, torso, left, right, bottom;
+var head_i, torso_i, left_i, right_i, bottom_i;
 
 /* ------------------------- P5.JS DEFAULTS ------------------------- */
 
@@ -14,23 +15,23 @@ function preload() {
 
     // head
     heads[0] = loadImage("assets/cat_head.png");
-    heads[1] = loadImage("assets/bowlingball_head.png");
-    heads[2] = loadImage("assets/dog_head.png");
-    heads[3] = loadImage("assets/fish_head.png");
+    heads[1] = loadImage("assets/dog_head.png");
+    heads[2] = loadImage("assets/real_pinocchio_head.png");
+    heads[3] = loadImage("assets/pinocchio_head.png");
     heads[4] = loadImage("assets/old_man_head.png");
-    heads[5] = loadImage("assets/pinocchio_head.png");
-    heads[6] = loadImage("assets/real_pinocchio_head.png");
-    
+    heads[5] = loadImage("assets/fish_head.png");
+    heads[6] = loadImage("assets/bowlingball_head.png");
+
     // torso
     torsos[0] = loadImage("assets/cat_torso.png");
-    torsos[1] = loadImage("assets/bikini_torso.png");
-    torsos[2] = loadImage("assets/bikini_piercings_torso.png");
+    torsos[1] = loadImage("assets/bikini_piercings_torso.png");
+    torsos[2] = loadImage("assets/bikini_torso.png");
     torsos[3] = loadImage("assets/buff_piercings_torso.png");
     torsos[4] = loadImage("assets/buff_torso.png");
-    torsos[5] = loadImage("assets/free_donuts_torso.png");
+    torsos[5] = loadImage("assets/swimmer_torso.png");
     torsos[6] = loadImage("assets/gingerbread_torso.png");
-    torsos[7] = loadImage("assets/gingerbread_torso.png");
-    torsos[8] = loadImage("assets/swimmer_torso.png");
+    torsos[7] = loadImage("assets/gingerbread_torso_eaten.png");
+    torsos[8] = loadImage("assets/free_donuts_torso.png");
 
     // bottom
     bottoms[0] = loadImage("assets/cat_bottom.png");
@@ -42,20 +43,20 @@ function preload() {
 
     // right
     rights[0] = loadImage("assets/cat_right.png");
-    rights[1] = loadImage("assets/dab_right.png");
-    rights[2] = loadImage("assets/goodhotdog_right.png");
-    rights[3] = loadImage("assets/octopus_right.png");
-    rights[4] = loadImage("assets/sadweiner_right.png");
-    rights[5] = loadImage("assets/watch_right.png");
-    rights[6] = loadImage("assets/tie_right.png");
+    rights[1] = loadImage("assets/goodhotdog_right.png");
+    rights[2] = loadImage("assets/dab_right.png");
+    rights[3] = loadImage("assets/tie_right.png");
+    rights[4] = loadImage("assets/watch_right.png");
+    rights[5] = loadImage("assets/sadweiner_right.png");
+    rights[6] = loadImage("assets/octopus_right.png");
 
     // left
     lefts[0] = loadImage("assets/cat_left.png");
-    lefts[1] = loadImage("assets/dab_left.png");
-    lefts[2] = loadImage("assets/donut_left.png");
-    lefts[3] = loadImage("assets/finger_left.png");
+    lefts[1] = loadImage("assets/donut_left.png");
+    lefts[2] = loadImage("assets/dab_left.png");
+    lefts[3] = loadImage("assets/snap_left.png");
     lefts[4] = loadImage("assets/selfie_left.png");
-    lefts[5] = loadImage("assets/snap_left.png");
+    lefts[5] = loadImage("assets/finger_left.png");
 }
 
 function setup() {
@@ -67,11 +68,17 @@ function setup() {
     textAlign(CENTER, CENTER);
     noStroke();
 
-    head = random(heads);
-    torso = random(torsos);
-    right = random(rights);
-    left = random(lefts);
-    bottom = random(bottoms);
+    head_i = int(random(heads.length()));
+    torso_i = int(random(torsos.length()));
+    right_i = int(random(rights.length()));
+    left_i = int(random(lefts.length()));
+    bottom_i = int(random(bottoms.length()));
+
+    head = heads[head_i];
+    torso = torsos[torso_i];
+    right = rights[right_i];
+    left = lefts[left_i];
+    bottom = bottoms[bottom_i];
 }
 
 function draw() {
@@ -90,13 +97,67 @@ function draw() {
     text("x", width/4, height/2);
     text("o", 3*width/4, height/2);
 
-    image(bottom, width/2-301, height/2-79, bottom.width/4, bottom.height/4);
+    image(bottom, width/2-301, height/2-68, bottom.width/4, bottom.height/4);
     image(torso, width/2-151, height/2-189, torso.width/4, torso.height/4);
+    image(head, width/2-194, height/2-325, head.width/4, head.height/4);
     image(right, width/2-66, height/2-335, right.width/4, right.height/4);
     image(left, width/2-333, height/2-237, left.width/4, left.height/4);
-    image(head, width/2-194, height/2-325, head.width/4, head.height/4);
 }
 
 function mousePressed() {
+    // later differentiate between right and left side;
+    var choice = int(random(2));
+    var body_part = int(random(5));
+    if (choice == 0) { // "good" choice
+        if (body_part == 0) { // bottom
+            var bottom_i = int(random(0, bottom_i));
+            bottom = bottoms[bottom_i];
+        }
 
+        else if (body_part == 1) { // torso
+            var torso_i = int(random(0, torso_i));
+            torso = torsos[torso_i];
+        }
+
+        else if (body_part == 2) { // head
+            var head_i = int(random(0, head_i));
+            head = heads[head_i];
+        }
+
+        else if (body_part == 3) { // right
+            var right_i = int(random(0, right_i));
+            right = rights[right_i];
+        }
+
+        else { // left
+            var left_i = int(random(0, left_i));
+            left = lefts[left_i];
+        }
+    }
+    else { // bad choice
+        if (body_part == 0) { // bottom
+            var bottom_i = int(random(bottom_i+1, bottoms.length()));
+            bottom = bottoms[bottom_i];
+        }
+
+        else if (body_part == 1) { // torso
+            var torso_i = int(random(torso_i+1, torsos.length()));
+            torso = torsos[torso_i];
+        }
+
+        else if (body_part == 2) { // head
+            var head_i = int(random(head_i+1, heads.length()));
+            head = heads[head_i];
+        }
+
+        else if (body_part == 3) { // right
+            var right_i = int(random(right_i+1, rights.length()));
+            right = rights[right_i];
+        }
+
+        else { // left
+            var left_i = int(random(left_i+1, lefts.length()));
+            left = lefts[left_i];
+        }
+    }
 }
