fd153a9bcbf223b6c8490facb07c6356a0f80797
amywuwho <amy.wu1579@gmail.com>
2019-03-28 02:32:09 -0400

    load assets

---
+++ p5/god-gave-me/main.js
@@ -0,0 +1,75 @@
+
+/* ------------------------- P5.JS DEFAULTS ------------------------- */
+
+function preload() {
+    // image loads
+
+    // head
+    bowlingball_head = loadImage("assets/bowlingball_head.png");
+    cat_head = loadImage("assets/cat_head.png");
+    dog_head = loadImage("assets/dog_head.png");
+    fish_head = loadImage("assets/fish_head.png");
+    old_man_head = loadImage("assets/old_man_head.png");
+    pinocchio_head = loadImage("assets/pinocchio_head.png");
+    real_pinocchio_head = loadImage("assets/real_pinocchio_head.png");
+    
+    // torso
+    bikini_torso = loadImage("assets/bikini_torso.png");
+    bikini_piercings_torso = loadImage("assets/bikini_piercings_torso.png");
+    buff_piercings_torso = loadImage("assets/buff_piercings_torso.png");
+    buff_torso = loadImage("assets/buff_torso.png");
+    cat_torso = loadImage("assets/cat_torso.png");
+    free_donuts_torso = loadImage("assets/free_donuts_torso.png");
+    gingerbread_torso = loadImage("assets/gingerbread_torso.png");
+    gingerbread_eaten_torso = loadImage("assets/gingerbread_torso.png");
+    swimmer_torso = loadImage("assets/swimmer_torso.png");
+
+    // bottom
+    cat_bottom = loadImage("assets/cat_bottom.png");
+    legwarmers_bottom = loadImage("assets/legwarmers_bottom.png");
+    mermaid_bottom = loadImage("assets/mermaid_bottom.png");
+    michaeljackson_bottom = loadImage("assets/michaeljackson_bottom.png");
+    robot_bottom = loadImage("assets/robot_bottom.png");
+    towel_bottom = loadImage("assets/towel_bottom.png");
+
+    // right
+    cat_right = loadImage("assets/cat_right.png");
+    dab_right = loadImage("assets/dab_right.png");
+    goodhotdog_right = loadImage("assets/goodhotdog_right.png");
+    octopus_right = loadImage("assets/octopus_right.png");
+    sadweiner_right = loadImage("assets/sadweiner_right.png");
+    watch_right = loadImage("assets/watch_right.png");
+    tie_right = loadImage("assets/tie_right.png");
+
+    // left
+    cat_left = loadImage("assets/cat_left.png");
+    dab_left = loadImage("assets/dab_left.png");
+    donut_left = loadImage("assets/donut_left.png");
+    finger_left = loadImage("assets/finger_left.png");
+    selfie_left = loadImage("assets/selfie_left.png");
+    snap_left = loadImage("assets/snap_left.png");
+}
+
+function setup() {
+
+    // canvas and basic setup
+    cnv = createCanvas(windowWidth, windowHeight);
+    centerCanvas();
+    background(255, 255, 255);
+    fill(0, 0, 0);
+    textSize(50);
+    textAlign(CENTER, CENTER);
+    noStroke();
+
+    
+
+}
+
+function draw() {
+    background(255, 255, 255);
+
+}
+
+function mousePressed() {
+
+}
